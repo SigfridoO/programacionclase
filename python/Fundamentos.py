492e7c4ffc7e4663794fcfc141848476af6738d9
sigfrido <sigfrido_o@ardilluda.com.mx>
2024-08-28 08:41:12 -0600

    ctualizando archivos

---
+++ python/Fundamentos.py
@@ -0,0 +1,17 @@
+print('hola mundo mi nombre es "Sigfrido"') 
+print('saludos')
+
+
+
+print("============== variables ==============")
+edad = 20
+print(edad)
+
+altura = 1.65
+print(altura)
+
+nombre = "Eduardo"
+print(nombre)
+
+es_trabajador = True
+print(es_trabajador)
